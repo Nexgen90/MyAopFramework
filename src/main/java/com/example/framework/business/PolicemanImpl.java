8b3b67ef0bec9bc36203931f739be03372e8b4a1
nikolay.mikutskiy <nikolay.mikutskiy@nexign.com>
2020-05-28 17:29:14 +0300

    Add PostConstruct support in ObjectFactory

--- src/main/java/com/example/framework/business/PolicemanImpl.java
+++ src/main/java/com/example/framework/business/PolicemanImpl.java
@@ -1,6 +1,19 @@
 package com.example.framework.business;
 
+import com.example.framework.infrastructure.configurators.singletone.InjectByType;
+
+import javax.annotation.PostConstruct;
+
 public class PolicemanImpl implements Policeman {
+
+    @InjectByType
+    private Recommendator recommendator;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("<PolicemanImpl>: " + recommendator.getClass());
+    }
+
     @Override
     public void makePeopleLeaveRoom() {
         System.out.println("Говорит полиция: Пожалуйста, все покиньте помещение.");
--- src/main/java/com/example/framework/infrastructure/ObjectFactory.java
+++ src/main/java/com/example/framework/infrastructure/ObjectFactory.java
@@ -3,6 +3,9 @@ package com.example.framework.infrastructure;
 import com.example.framework.infrastructure.configurators.ObjectConfigurator;
 import lombok.SneakyThrows;
 
+import javax.annotation.PostConstruct;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,7 +25,16 @@ public class ObjectFactory {
     public <T> T createObject(Class<T> implClass) {
         T t = create(implClass);
         configure(t);
+        invokeInit(implClass, t);
         return t;
+    }
+
+    private <T> void invokeInit(Class<T> implClass, T t) throws IllegalAccessException, InvocationTargetException {
+        for (Method method : implClass.getMethods()) {
+            if (method.isAnnotationPresent(PostConstruct.class)) {
+                method.invoke(t);
+            }
+        }
     }
 
     private <T> void configure(T t) {
